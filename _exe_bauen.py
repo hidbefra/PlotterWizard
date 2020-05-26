e2987530cd74b747a1ca17f4fc073251901f9883
hidbefra <hidbefra@gmail.com>
2020-05-26 12:19:47 +0200

    add installer over Inno Setup

--- _exe_bauen.py
+++ _exe_bauen.py
@@ -5,3 +5,7 @@ import os
 # os.system("pyinstaller --onefile --noconsole PlotterWizard.py")
 
 os.system("pyinstaller --onefile --icon=icons\\laser_YXc_icon.ico PlotterWizard.py")
+
+
+# installer erstellen
+os.system("iscc dist\\PlotterWizard.iss")
--- _externe_inhalte_installieren.py
+++ _externe_inhalte_installieren.py
@@ -14,3 +14,5 @@ install("pyserial")
 # install("pyinstaller")
 install("https://github.com/pyinstaller/pyinstaller/archive/develop.zip")
 # https://stackoverflow.com/questions/37815371/pyinstaller-failed-to-execute-script-pyi-rth-pkgres-and-missing-packages
+
+#manuel https://jrsoftware.org/isdl.php instalieren und programm order in den Systemvariablne eintragen unter PATH
